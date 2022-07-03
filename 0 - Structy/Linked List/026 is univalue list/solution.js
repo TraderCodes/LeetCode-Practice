d0d8d69eb11eb1b4d0b110f0320acd650ffd784c
TraderCodes <TraderCodes@users.noreply.github.com>
2022-07-03 12:35:36 -0700

    test_04 [PASS] 67ms > 5/5 tests passed

---
+++ 0 - Structy/Linked List/026 is univalue list/solution.js
@@ -0,0 +1,22 @@
+// iterative;
+const isUnivalueList = (head) => {
+  let current = head;
+
+  while (current !== null) {
+    if (current.val !== head.val) return false;
+    current = current.next;
+  }
+
+  return true;
+};
+
+
+// recursive;
+const isUnivalueList = (head, prevVal = null) => {
+  if (head === null) return true;
+  if (prevVal === null || prevVal === head.val) {
+    return isUnivalueList(head.next, head.val);
+  } else {
+    return false;
+  }
+};
