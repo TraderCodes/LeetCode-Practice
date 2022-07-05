48911255ff71225c313889750eee3ce78df9d9ff
TraderCodes <TraderCodes@users.noreply.github.com>
2022-07-04 17:31:44 -0700

    done

---
+++ 0 - Structy/Binary tree/038 breadth first values/solution.js
@@ -0,0 +1,21 @@
+const breadthFirstValues = (root) => {
+  if (root === null) return [];
+
+  const values = [];
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    values.push(node.val);
+
+    if (node.left !== null) queue.push(node.left);
+
+    if (node.right !== null) queue.push(node.right);
+  }
+
+  return values;
+};
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+// Note: this solution should really be considered O(n^2) runtime because the JavaScript shift() methods runs in O(n). JavaScript does not have a native queue data structure that is maximally efficient.
