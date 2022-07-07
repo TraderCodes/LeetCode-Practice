df2bca107fe1337cb03100bbc8f63603ff7bf55f
TraderCodes <TraderCodes@users.noreply.github.com>
2022-07-06 19:30:13 -0700

    test_00 [PASS] 92ms test_01 [PASS] 71ms test_02 [PASS] 72ms test_03 [PASS] 74ms test_04 [PASS] 70ms test_05 [PASS] 68ms test_06 [PASS] 73ms > 7/7 tests passed

---
+++ 0 - Structy/Binary tree/043 tree path finder/solution.js
@@ -0,0 +1,47 @@
+depth first
+const pathFinder = (root, target) => {
+  if (root === null) return null;
+  if (root.val === target) return [ root.val ];
+  
+  const leftPath = pathFinder(root.left, target);
+  if (leftPath !== null) return [ root.val, ...leftPath];
+  
+  const rightPath = pathFinder(root.right, target);
+  if (rightPath !== null) return [ root.val, ...rightPath];
+  
+  return null;
+};
+n = number of nodes
+Time: O(n^2)
+Space: O(n)
+depth first w/ push
+const pathFinder = (root, target) => {
+  const result = pathFinderHelper(root, target);
+  if (result === null) {
+    return null;
+  } else {
+    return result.reverse();
+  }
+};
+
+const pathFinderHelper = (root, target) => {
+  if (root === null) return null;
+  if (root.val === target) return [ root.val ];
+  
+  const leftPath = pathFinderHelper(root.left, target);
+  if (leftPath !== null) {
+    leftPath.push(root.val);
+    return leftPath;
+  }
+  
+  const rightPath = pathFinderHelper(root.right, target);
+  if (rightPath !== null) {
+    rightPath.push(root.val);
+    return rightPath;
+  }
+  
+  return null;
+};
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
