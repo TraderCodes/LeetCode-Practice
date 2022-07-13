ec4cd84f12ee53836bfa2508876b1181599c8309
TraderCodes <TraderCodes@users.noreply.github.com>
2022-07-12 20:37:11 -0700

    test_00 [PASS] 76ms test_01 [PASS] 72ms test_02 [PASS] 81ms test_03 [PASS] 77ms test_04 [PASS] 99ms test_05 [PASS] 76ms > 6/6 tests passed

---
+++ 0 - Structy/Graph/064 longest path/solution.js
@@ -0,0 +1,33 @@
+solutions
+depth first
+const longestPath = (graph) => {
+  const distance = {};
+  for (let node in graph) {
+    if (graph[node].length === 0) {
+      distance[node] = 0;
+    }
+  }
+
+  for (let node in graph) {
+    traverseDistance(graph, node, distance);
+  }
+
+  return Math.max(...Object.values(distance));
+};
+
+const traverseDistance = (graph, node, distance) => {
+  if (node in distance) return distance[node];
+
+  let maxDistance = 0;
+  for (let neighbor of graph[node]) {
+    const attempt = traverseDistance(graph, neighbor, distance);
+    if (attempt > maxDistance) maxDistance = attempt;
+  }
+
+  distance[node] = 1 + maxDistance;
+  return distance[node];
+};
+// e = # edges
+// n = # nodes
+// Time: O(e)
+// Space: O(n)
