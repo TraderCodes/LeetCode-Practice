4154be163eaf9cfe0511b003a1095ce29ab25575
TraderCodes <TraderCodes@users.noreply.github.com>
2022-07-20 20:56:06 -0700

    test_00 [PASS] 82ms test_01 [PASS] 69ms test_02 [PASS] 70ms test_03 [PASS] 78ms test_04 [PASS] 68ms > 5/5 tests passed

---
+++ 0 - Structy/Dynamic Programming/078 non-adjacent sum/solution.js
@@ -0,0 +1,14 @@
+dynamic programming with memoization
+const nonAdjacentSum = (nums, i = 0, memo = {}) => {
+  if (i in memo) return memo[i];
+  
+  if (i >= nums.length) return 0;
+  
+  const include = nums[i] + nonAdjacentSum(nums, i + 2, memo);
+  const exclude = nonAdjacentSum(nums, i + 1, memo);
+  memo[i] = Math.max(include, exclude);
+  return memo[i]
+};
+n = length of nums
+Time: O(n)
+Space: O(n)
